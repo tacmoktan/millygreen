1d1188b2860d31b393523134984973342f121a26
Tashi <tacmoktan@gmail.com>
2019-11-21 20:21:31 +0545

    Update css and script for slick

--- js/slick.js
+++ js/slick.js
@@ -1,8 +1,18 @@
+/* front-page sliders */
 $(document).ready(function () {
     $('.carousel').slick({
-        arrows:false,
-        dots:true,
+        arrows: false,
+        dots: true,
         autoplay: true,
         autoplaySpeed: 2000
+    });
+});
+
+/* best selling products sliders */
+$(document).ready(function () {
+    $('.carousel-best-selling-products').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
     });
 });
