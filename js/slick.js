efd4b9b3078ba8a1d6adfc9cc9df9eaf5fc089d3
Tashi <tacmoktan@gmail.com>
2019-11-22 14:14:59 +0545

    Add styles in Best Selling Product section

--- js/slick.js
+++ js/slick.js
@@ -11,8 +11,10 @@ $(document).ready(function () {
 /* best selling products sliders */
 $(document).ready(function () {
     $('.carousel-best-selling-products').slick({
-        infinite: true,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        variableWidth:true
     });
 });
